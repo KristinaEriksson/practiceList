c2ad22761e18cc660ff507851c946ea2473158cb
Kristina Eriksson <wetonen@gmail.com>
2022-10-20 15:45:13 +0200

    Add project files.

---
+++ practiceList/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practiceList
+{
+    class Country
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Capital { get; set; }
+
+    }
+}
---
+++ practiceList/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace practiceList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country C1 = new Country() { Name = "Sweden", Code =  "SWE", Capital = "Stockholm" };
+            Country C2 = new Country() { Name = "Findland", Code =  "FIN", Capital = "Helsinki" };
+            Country C3 = new Country() { Name = "Denmark", Code =  "DK", Capital = "Köpenhamn" };
+            Country C4 = new Country() { Name = "Poland", Code =  "POL", Capital = "Warszawa" };
+            Country C5 = new Country() { Name = "Iceland", Code =  "ICE", Capital = "Reykjavik" };
+
+            List<Country> listCountry = new List<Country>();
+            listCountry.Add(C1);
+            listCountry.Add(C2);
+            listCountry.Add(C3);
+            listCountry.Add(C4);
+            listCountry.Add(C5);
+
+            string userInput = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Please Enter Country Code: ");
+                string ucc = Console.ReadLine();
+                Country resultcountry = listCountry.Find(c => c.Code == ucc);
+                if (resultcountry == null)
+                {
+                    Console.WriteLine("Country Code is not valid.");
+                }
+                else
+                {
+                    Console.WriteLine("Country Code is valid.");
+                }
+                do
+                {
+                    Console.WriteLine("Do you want to continue? (Y/N) ");
+                    userInput = Console.ReadLine();
+                }
+                while (userInput != "N" && userInput != "Y");
+
+                {
+
+                }
+            }
+            while (userInput == "Y");
+
+            {
+
+            }
+        }
+    }
+}
